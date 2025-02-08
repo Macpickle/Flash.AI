30dc32a104a8a695ec5aec9760bc978704d1d93f
Dylan MacLeod <dillpicklemac@icloud.com>
2025-02-08 13:01:10 -0500

    updated home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,16 @@
 import NavBar from '../components/Navbar';
 import { GoGear } from "react-icons/go";
-import { IoMdAdd } from "react-icons/io";
+import { MdDelete } from "react-icons/md";
 import { IoMdCreate } from "react-icons/io";
 
 const testCard = (index) => {
     return (
         <div className="card doc m-2" key={index} style={{width: '20dvw', height: '20dvw'}}>
+            <div className="hide">
+                <button className = "transparent-button"><IoMdCreate className="icon" size={30} /></button>
+                <button className = "transparent-button"><MdDelete className="icon" size={30} /></button>
+            </div>
+
             <div className="card-body d-flex flex-column justify-content-center align-items-center">
                 <h5 className="card-title">Sample Title</h5>
                 <p className="card-text">01/01/2023</p>
@@ -24,8 +29,6 @@ function Home() {
             <div className="container-fluid d-flex flex-column align-items-center" style = {{padding: '1%'}}>
                 <div className="search-bar w-75 d-flex align-items-center">
                     <input type="text" placeholder="Search..." className="form-control me-2" />
-                    <button className="button me-2"><IoMdCreate size={30} /></button>
-                    <button className="button"><IoMdAdd size={30} /></button>
                 </div>
             </div>
 
