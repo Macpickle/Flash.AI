ed4ed60562533121189c04af6884e3aaa240a4fb
Dylan MacLeod <dillpicklemac@icloud.com>
2025-02-09 11:36:23 -0500

    added image

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -2,11 +2,24 @@ import NavBar from '../components/Navbar';
 
 function Landing() {
     return (
-        <div>
+        <div className="landing" style={{ height: '100vh', overflow: 'hidden' }}>
             <NavBar>
                 <a href="/register" className="button">Sign Up</a>
             </NavBar>
-            
+        
+            <div className="container main" style={{ height: '100%', overflow: 'hidden' }}>
+                <div className="computer position-absolute mt-5" style={{ marginLeft: "-25%" }}>   
+                    <h1 className="display-1">Flash.AI</h1>
+                    <p className="lead">Upload. Quiz. Master. Ace Your Exams with Flash.AI!</p>
+                    <a href="/login" className="button">Get Started</a>
+                </div>
+                <div className="phone container-fluid" style={{ textAlign: 'center' }}>
+                    <h1 className="display-1">Flash.AI</h1>
+                    <p className="lead">Upload. Quiz. Master. Ace Your Exams with Flash.AI!</p>
+                    <a href="/login" className="button">Get Started</a>
+                </div>
+                <div className="cutoff"></div>
+            </div>
         </div>
     );
 }
