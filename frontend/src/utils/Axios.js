bca473fcbefa323080601e98d4598171c43110e8
Jack <omnomnomny@gmail.com>
2025-02-16 16:01:12 -0500

    fixed api url handling from .env

--- frontend/src/utils/Axios.js
+++ frontend/src/utils/Axios.js
@@ -1,27 +1,28 @@
-import axios from 'axios';
-const node_environment = import.meta.env.VITE_NODE_ENV; // gets the environment of the app
+import axios from "axios";
+const NODE_ENV = import.meta.env.VITE_NODE_ENV; // gets the environment of the app
+const API_URL = import.meta.env.VITE_API_URL; // gets the environment of the app
 
 // Axios interceptor to add the token to the request headers
 axios.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
 );
 
 // Axios request function, takes in an object with url, method, and data
-function AxiosRequest({url, method = 'get', data = {}}) {
-    return axios({
-        method: method,
-        url: `${node_environment === 'development' ? 'http://localhost:3000' : ''}${url}`,
-        data: data
-    });
+function AxiosRequest({ url, method = "get", data = {} }) {
+  return axios({
+    method: method,
+    url: `${NODE_ENV === "development" ? "http://localhost:3000" : API_URL}${url}`,
+    data: data,
+  });
 }
 
 export default AxiosRequest;
