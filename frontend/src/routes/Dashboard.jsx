8fd9038ae96c0c826fceeaa70058bb0cc2ca89fa
Jack <omnomnomny@gmail.com>
2025-02-16 05:00:14 -0500

    idk why that line was even there

--- frontend/src/routes/Dashboard.jsx
+++ frontend/src/routes/Dashboard.jsx
@@ -3,7 +3,6 @@ import AppNavbar from "@/components/app-navbar";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-// import { Card, CardContent } from "@/components/ui/card";
 import {
   Select,
   SelectContent,
