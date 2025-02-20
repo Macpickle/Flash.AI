045f7871c8b0e90e7c5aa92e238f8da57a896b47
Dylan MacLeod <dillpicklemac@icloud.com>
2025-02-19 23:40:03 -0500

    updated sidenav, much cleaner, more accessible, just need to decide how to do darkmode

--- frontend/src/components/app-sidenav.jsx
+++ frontend/src/components/app-sidenav.jsx
@@ -38,7 +38,7 @@ const createItems = [
 
 // sample user for now
 const user = {
-  username: "John Doe",
+  username: "the Goat",
   image: "https://blackwonder.tf/attachments/1673671146282-png.31249/",
 };
 
@@ -56,112 +56,96 @@ function SideNav({ handleCreate, handleCollapse, isCollapsed, screenSize }) {
 
   return (
     <div className="z-50 flex-shrink-0">
-        <div
-          className={`flex flex-col bg-black border-r sticky border-gray-700 text-white h-screen ${
-            isCollapsed ? "w-16" : "w-64"
-          } transition-all duration-300 ease-in-out`}
-        >
-          {screenSize === "large" && (
-            <div className="flex justify-end p-4">
-              <Button
-                variant="ghost"
-                size="icon"
-                onClick={() => handleCollapse()}
-                aria-label={isCollapsed ? "Expand sidebar" : "Collapse sidebar"}
-              >
-                {isCollapsed ? (
-                  <ChevronRight className="h-4 w-4" />
-                ) : (
-                  <ChevronLeft className="h-4 w-4" />
-                )}
-              </Button>
-            </div>
-          )}
-          <div className="flex items-center p-4 space-x-2">
-            <img
-              src={user.image}
-              alt="User"
-              className="w-8 h-8 rounded-full object-cover"
-            />
-            {!isCollapsed && <span className="text-lg min-w-[100px]">{user.username}</span>}
-          </div>
-          <ScrollArea className="flex-grow">
-            <nav className="space-y-2 p-2">
-              {navItems.map((item) =>
-                item.type === "link" ? (
-                  <Link
-                    key={item.name}
-                    to={item.href}
-                    className="flex items-center space-x-2 rounded-lg px-3 py-2 text-gray-200 hover:bg-neutral-800"
-                  >
-                    <item.icon className="h-5 w-5" />
-                    {!isCollapsed && <span>{item.name}</span>}
-                  </Link>
-                ) : item.type === "button" ? (
-                  <Button
-                    key={item.name}
-                    variant="ghost"
-                    size="icon"
-                    className="flex items-center justify-center space-x-2 text-gray-200 hover:bg-neutral-800"
-                    onClick={() => handleCreate(item.type)}
-                  >
-                    <item.icon className="h-5 w-5" />
-                    {!isCollapsed && <span>{item.name}</span>}
-                  </Button>
-                ) : (
-                  item.type === "dropdown" && (
-                    <Select
-                      key={item.name}
-                      className="border border-none"
-                      onValueChange={(value) => handleCreate(value.type)}
-                    >
-                      <SelectTrigger className="border border-none dark:border-gray-700 hover:bg-neutral-800">
-                        <div className="flex items-center justify-center space-x-2 text-gray-200 hover:bg-neutral-800">
-                          <item.icon className="h-5 w-5" />
-                          {!isCollapsed && <span>{item.name}</span>}
-                        </div>
-                      </SelectTrigger>
-                      <SelectContent className="bg-black border border-gray-700 dark:border-gray-700 rounded-lg text-white">
-                        {createItems.map((createItem) => (
-                          <SelectItem
-                            key={createItem.name}
-                            value={createItem}
-                            className="hover:bg-neutral-800 cursor-pointer"
-                          >
-                            {createItem.name}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                  )
-                ),
-              )}
-            </nav>
-          </ScrollArea>
-          <div className="p-4 flex flex-col items-center">
+      <div className={`flex flex-col bg-black border-r sticky border-gray-700 text-white h-screen ${isCollapsed ? "w-16" : "w-64"} transition-all duration-300 ease-in-out`}>
+        {screenSize === "large" && (
+          <div className="flex justify-end p-4">
             <Button
               variant="ghost"
               size="icon"
-              onClick={toggleDarkMode}
-              aria-label="Toggle dark mode"
+              onClick={() => handleCollapse()}
+              aria-label={isCollapsed ? "Expand sidebar" : "Collapse sidebar"}
             >
-              {isDarkMode ? (
-                <LuSun className="h-5 w-5" />
+              {isCollapsed ? (
+                <ChevronRight className="h-4 w-4" />
               ) : (
-                <LuMoon className="h-5 w-5" />
+                <ChevronLeft className="h-4 w-4" />
               )}
             </Button>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="w-full flex items-center justify-center space-x-2 text-gray-200 hover:bg-neutral-800 hover:text-white"
-              aria-label="Logout"
-            >
-              <LogOut className="h-5 w-5" />
-              {!isCollapsed && <span>Logout</span>}
-            </Button>
           </div>
+        )}
+
+        <div className="flex flex-col items-center p-4 space-x-2">
+          <img
+            src={user.image}
+            alt="User"
+            className={`rounded-full object-center object-cover transition-all duration-300 ease-in-out ${isCollapsed ? "w-8 h-8" : "w-16 h-16"}`}
+          />
+          {!isCollapsed && <span className="text-2xl min-w-[100px]">{user.username}</span>}
         </div>
+
+        <ScrollArea className="flex-grow">
+          <nav className={`flex justify-center flex-col space-y-2 p-2 ${isCollapsed ? "items-center px-0" : "px-2"}`}>
+            {navItems.map((item) =>
+              item.type === "link" ? (
+                <Link
+                  key={item.name}
+                  to={item.href}
+                  className="flex items-center rounded-lg space-x-2 px-3 py-2 text-gray-200 hover:bg-neutral-800"
+                >
+                  <item.icon className="h-5 w-5" />
+                  {!isCollapsed && <span>{item.name}</span>}
+                </Link>
+              ) : item.type === "dropdown" ? (
+                item.type === "dropdown" && (
+                  <Select
+                    className="border border-none"
+                    onValueChange={(value) => handleCreate(value.type)}
+                    key={item.name}
+                  >
+                    <SelectTrigger className="border border-none dark:border-gray-700 hover:bg-neutral-800" isArrow={false}>
+                      <div className="flex items-center justify-center space-x-2 text-gray-200 hover:bg-neutral-800">
+                        <item.icon className="h-5 w-5" />
+                        {!isCollapsed && <span>{item.name}</span>}
+                      </div>
+                    </SelectTrigger>
+                    <SelectContent className="bg-black border border-gray-700 dark:border-gray-700 rounded-lg text-white">
+                      {createItems.map((createItem) => (
+                        <SelectItem
+                          key={createItem.name}
+                          value={createItem}
+                          className="hover:bg-neutral-800 cursor-pointer"
+                        >
+                          {createItem.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                )
+              ) : (
+                <></>
+              ),
+            )}
+          </nav>
+        </ScrollArea>
+
+        <div className="px-4 py-2 space-y-2 flex flex-col items-center">
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={toggleDarkMode}
+            aria-label="Toggle dark mode"
+          >
+            {isDarkMode ? ( <LuSun className="h-5 w-5" /> ) : ( <LuMoon className="h-5 w-5" /> )}
+          </Button>
+          <Link
+            to="/logout"
+            className="w-full items-center rounded-lg space-x-2 px-3 py-2 text-gray-200 hover:bg-neutral-800 flex"
+          >
+            <LogOut className="h-5 w-5" />
+            {!isCollapsed && <span>Logout</span>}
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
